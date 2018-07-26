d50b339c841f40bfaea40a7ba2c70b732aca8d94
qingjun <qingjun@bytedance.com>
2018-07-26 17:55:13 +0800

    test

---
+++ test/test_1.sh
@@ -0,0 +1,2 @@
+docker pull training/webapp
+docker run -d -P training/webapp python app.py
---
+++ test/test_python.py
@@ -0,0 +1,6 @@
+import docker
+
+docker.api()
+#client = docker.from_env()
+#client.containers.run("ubuntu","echo hello world")
+
